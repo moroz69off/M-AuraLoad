7e59f142c8749c2727bb5296910ed270b5bb70e3
moroz69off <moroz69off@gmail.com>
2021-02-13 13:03:11 +0300

    add human obj model

--- M-AuraLoad-F7/AForm.Designer.cs
+++ M-AuraLoad-F7/AForm.Designer.cs
@@ -30,21 +30,44 @@ namespace M_AuraLoad_F7
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AForm));
+            this.sceneControl = new SharpGL.SceneControl();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneControl)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // sceneControl
+            // 
+            this.sceneControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sceneControl.DrawFPS = false;
+            this.sceneControl.Location = new System.Drawing.Point(0, 0);
+            this.sceneControl.Name = "sceneControl";
+            this.sceneControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.sceneControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.sceneControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.sceneControl.Size = new System.Drawing.Size(914, 614);
+            this.sceneControl.TabIndex = 0;
+            this.sceneControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.sceneControl_OpenGLDraw);
+            this.sceneControl.Load += new System.EventHandler(this.sceneControl_Load);
+            this.sceneControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.sceneControl_MouseDown);
+            this.sceneControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.sceneControl_MouseMove);
+            this.sceneControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.sceneControl_MouseUp);
+            // 
+            // AForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 614);
+            this.Controls.Add(this.sceneControl);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "AForm";
             this.Text = "Aura load";
+            ((System.ComponentModel.ISupportInitialize)(this.sceneControl)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private SharpGL.SceneControl sceneControl;
     }
 }
 
--- M-AuraLoad-F7/AForm.cs
+++ M-AuraLoad-F7/AForm.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Effects;
+using SharpGL.Serialization;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,9 +15,57 @@ namespace M_AuraLoad_F7
 {
     public partial class AForm : Form
     {
+        private bool isMale = Properties.Settings.Default.IsMale;
+        private ArcBallEffect arcBallEffect = new ArcBallEffect();
+        private string path;
+
         public AForm()
         {
             InitializeComponent();
+            LoadHuman(isMale);
+        }
+
+        private void LoadHuman(bool isMale)
+        {
+            if (isMale)
+                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "male.obj");
+            else path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "female.obj");
+            Scene scene = SerializationEngine.Instance.LoadScene(path);
+        }
+
+        /// <summary>
+        /// Load scene control in windows form
+        /// </summary>
+        /// <param name="sender">Scene control</param>
+        /// <param name="e">System.EventArgs</param>
+        private void sceneControl_Load(object sender, EventArgs e)
+        {
+            object testSender = sender;
+        }
+        #region mouse events
+        private void sceneControl_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void sceneControl_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void sceneControl_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+        #endregion mouse events
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">Scene control</param>
+        /// <param name="args">System.Drawing.Graphics</param>
+        private void sceneControl_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            args.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
         }
     }
 }
