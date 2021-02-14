22c7632da32b1d35dc2abd30918024b7279d0607
moroz69off <moroz69off@gmail.com>
2021-02-14 21:54:56 +0300

    male human ok

--- M-AuraLoad-F7/AForm.cs
+++ M-AuraLoad-F7/AForm.cs
@@ -1,6 +1,12 @@
-﻿using SharpGL.SceneGraph;
+﻿using SharpGL;
+using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Assets;
+using SharpGL.SceneGraph.Cameras;
+using SharpGL.SceneGraph.Core;
 using SharpGL.SceneGraph.Effects;
+using SharpGL.SceneGraph.Primitives;
 using SharpGL.Serialization;
+using SharpGL.Serialization.Wavefront;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,19 +24,13 @@ namespace M_AuraLoad_F7
         private bool isMale = Properties.Settings.Default.IsMale;
         private ArcBallEffect arcBallEffect = new ArcBallEffect();
         private string path;
+        private List<Polygon> polygons = new List<Polygon>();
+        private float rotate = 0;
 
         public AForm()
         {
             InitializeComponent();
             LoadHuman(isMale);
-        }
-
-        private void LoadHuman(bool isMale)
-        {
-            if (isMale)
-                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "male.obj");
-            else path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "female.obj");
-            Scene scene = SerializationEngine.Instance.LoadScene(path);
         }
 
         /// <summary>
@@ -40,32 +40,75 @@ namespace M_AuraLoad_F7
         /// <param name="e">System.EventArgs</param>
         private void sceneControl_Load(object sender, EventArgs e)
         {
-            object testSender = sender;
+            // Remove red bounding box of models
+            sceneControl.Scene.RenderBoundingVolumes =
+                !sceneControl.Scene.RenderBoundingVolumes;
+            // Remove default axies
+            sceneControl.Scene.SceneContainer.Children[0]
+                .RemoveChild(sceneControl.Scene.SceneContainer.Children[0].Children[1]);
+            // Remove default Grid
+            sceneControl.Scene.SceneContainer.Children[0]
+                .RemoveChild(sceneControl.Scene.SceneContainer.Children[0].Children[0]);
+
+            sceneControl.Scene.CurrentCamera.Position = new Vertex(0, -12.345f, 0);
+            sceneControl.Scene.SceneContainer.Effects.Add(arcBallEffect);
+            sceneControl.OpenGL.Enable(OpenGL.GL_TEXTURE_2D);
         }
+
+        private void LoadHuman(bool isMale)
+        {
+            if (isMale) path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "maleMin.obj");
+            else path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "female.obj");
+            ObjFileFormat obj = new ObjFileFormat();
+            Scene sceneHumanObject = obj.LoadData(path);
+            foreach (Asset asset in sceneHumanObject.Assets) sceneControl.Scene.Assets.Add(asset);
+            polygons = sceneHumanObject.SceneContainer.Traverse<Polygon>().ToList();
+            foreach (Polygon polygon in polygons)
+            {
+                polygon.Name = "HUMAN";
+                BoundingVolume boundingVolume = polygon.BoundingVolume;
+                var extent = new float[3];
+                boundingVolume.GetBoundDimensions(out extent[0], out extent[1], out extent[2]);
+                float maxExtent = extent.Max();
+                float scaleFactor = maxExtent > 10 ? 10.0f / maxExtent : 1;
+                polygon.Parent.RemoveChild(polygon);
+                polygon.Transformation.RotateX = 180; // 
+                polygon.Transformation.ScaleX = scaleFactor;
+                polygon.Transformation.ScaleY = scaleFactor;
+                polygon.Transformation.ScaleZ = scaleFactor;
+                polygon.Freeze(sceneControl.OpenGL);
+                sceneControl.Scene.SceneContainer.AddChild(polygon);
+                polygon.AddEffect(new OpenGLAttributesEffect());
+                //polygon.AddEffect(arcBallEffect);
+            }
+        }
+
         #region mouse events
         private void sceneControl_MouseDown(object sender, MouseEventArgs e)
         {
-
+            arcBallEffect.ArcBall.SetBounds(sceneControl.Width, sceneControl.Height);
+            arcBallEffect.ArcBall.MouseDown(e.X/2, e.Y/2);
         }
 
         private void sceneControl_MouseMove(object sender, MouseEventArgs e)
         {
-
+            if (e.Button == MouseButtons.Left) arcBallEffect.ArcBall.MouseMove(e.X/2, e.Y/2);
         }
 
         private void sceneControl_MouseUp(object sender, MouseEventArgs e)
         {
-
+            arcBallEffect.ArcBall.MouseUp(e.X/2, e.Y/2);
         }
         #endregion mouse events
+
         /// <summary>
-        /// 
+        /// Draw scene in framerate resolution
         /// </summary>
         /// <param name="sender">Scene control</param>
         /// <param name="args">System.Drawing.Graphics</param>
         private void sceneControl_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
         {
-            args.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            rotate += 5f;
         }
     }
 }
