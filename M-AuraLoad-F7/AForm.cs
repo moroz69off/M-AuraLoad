0ac210c21a194dfaaf11c7590e1167e619f14c15
moroz69off <moroz69off@gmail.com>
2021-02-18 00:31:35 +0300

    cylinders ok

--- M-AuraLoad-F7/AForm.cs
+++ M-AuraLoad-F7/AForm.cs
@@ -104,16 +104,111 @@ namespace M_AuraLoad_F7
         /// </summary>
         private void LoadAura()
         {
+            Cylinder cylP_r  = new Cylinder(),
+                     cylGI_r = new Cylinder(),
+                     cylE_r  = new Cylinder(),
+                     cylRP_r = new Cylinder(),
+                     cylC_r  = new Cylinder(),
+                     cylIG_r = new Cylinder(),
+                     cylV_r  = new Cylinder(),
+                     cylR_r  = new Cylinder(),
+                     cylMC_r = new Cylinder(),
+                     cylTR_r = new Cylinder(),
+                     cylVB_r = new Cylinder(),
+                     cylF_r  = new Cylinder(),
+                     cylP_l  = new Cylinder(),
+                     cylGI_l = new Cylinder(),
+                     cylE_l  = new Cylinder(),
+                     cylRP_l = new Cylinder(),
+                     cylC_l  = new Cylinder(),
+                     cylIG_l = new Cylinder(),
+                     cylV_l  = new Cylinder(),
+                     cylR_l  = new Cylinder(),
+                     cylMC_l = new Cylinder(),
+                     cylTR_l = new Cylinder(),
+                     cylVB_l = new Cylinder(),
+                     cylF_l  = new Cylinder();
 
-            Sphere sphereAura = new Sphere();
-            sphereAura.QuadricDrawStyle = DrawStyle.Line;
-            sphereAura.Transformation.ScaleX = 4f;
-            sphereAura.Transformation.ScaleY = 4f;
-            sphereAura.Transformation.ScaleZ = 6f;
-            sphereAura.AddEffect(arcBallEffect);
-            int stacks = sphereAura.Stacks;
-            sphereAura.Material = auraMaterial;
-            sceneControl.Scene.SceneContainer.AddChild(sphereAura);
+            List<Cylinder> cylinders = new List<Cylinder>(24);
+
+            cylinders.Add(cylP_r );
+            cylinders.Add(cylGI_r);
+            cylinders.Add(cylE_r );
+            cylinders.Add(cylRP_r);
+            cylinders.Add(cylC_r );
+            cylinders.Add(cylIG_r);
+            cylinders.Add(cylV_r );
+            cylinders.Add(cylR_r );
+            cylinders.Add(cylMC_r);
+            cylinders.Add(cylTR_r);
+            cylinders.Add(cylVB_r);
+            cylinders.Add(cylF_r );
+            cylinders.Add(cylP_l );
+            cylinders.Add(cylGI_l);
+            cylinders.Add(cylE_l );
+            cylinders.Add(cylRP_l);
+            cylinders.Add(cylC_l );
+            cylinders.Add(cylIG_l);
+            cylinders.Add(cylV_l );
+            cylinders.Add(cylR_l );
+            cylinders.Add(cylMC_l);
+            cylinders.Add(cylTR_l);
+            cylinders.Add(cylVB_l);
+            cylinders.Add(cylF_l );
+
+            cylP_r.Name  = "cylP_r";
+            cylGI_r.Name = "cylGI_r";
+            cylE_r.Name  = "cylE_r";
+            cylRP_r.Name = "cylRP_r";
+            cylC_r.Name  = "cylC_r";
+            cylIG_r.Name = "cylIG_r";
+            cylV_r.Name  = "cylV_r";
+            cylR_r.Name  = "cylR_r";
+            cylMC_r.Name = "cylMC_r";
+            cylTR_r.Name = "cylTR_r";
+            cylVB_r.Name = "cylVB_r";
+            cylF_r.Name  = "cylF_r";
+            cylP_l.Name  = "cylP_l";
+            cylGI_l.Name = "cylGI_l";
+            cylE_l.Name  = "cylE_l";
+            cylRP_l.Name = "cylRP_l";
+            cylC_l.Name  = "cylC_l";
+            cylIG_l.Name = "cylIG_l";
+            cylV_l.Name  = "cylV_l";
+            cylR_l.Name  = "cylR_l";
+            cylMC_l.Name = "cylMC_l";
+            cylTR_l.Name = "cylTR_l";
+            cylVB_l.Name = "cylVB_l";
+            cylF_l.Name  = "cylF_l";
+
+            foreach (Cylinder cylinder in cylinders)
+            {
+                cylinder.Material = auraMaterial;
+                cylinder.AddEffect(arcBallEffect);
+                //cylinder.TopRadius  = 1f;
+                //cylinder.BaseRadius = 1f;
+                cylinder.Slices = 24;
+                cylinder.Stacks = 1;
+                cylinder.Height = .1;
+                cylinder.QuadricDrawStyle = DrawStyle.Line;
+                cylinder.Transformation.ScaleX = 4f;
+                cylinder.Transformation.ScaleY = 4f;
+                cylinder.Transformation.ScaleZ = 4f;
+            }
+
+            for (int i = 0; i < 24; i++)
+            {
+                sceneControl.Scene.SceneContainer.AddChild(cylinders[i]);
+            }
+
+            for (int i = 12; i >= 1; i--)
+            {
+                int r = Math.Abs(i - 11);
+                cylinders[i].TopRadius = r / 10f;
+                cylinders[i + 11].TopRadius = r / 10f;
+                cylinders[i].Transformation.TranslateZ = i/2.5f;
+                cylinders[i + 11].Transformation.TranslateZ = -i/2.5f;
+            }
         }
 
         #region mouse events
@@ -149,3 +244,12 @@ namespace M_AuraLoad_F7
         }
     }
 }
+//Sphere sphereAura = new Sphere();
+//sphereAura.QuadricDrawStyle = DrawStyle.Line;
+//sphereAura.Transformation.ScaleX = 4f;
+//sphereAura.Transformation.ScaleY = 4f;
+//sphereAura.Transformation.ScaleZ = 6f;
+//sphereAura.AddEffect(arcBallEffect);
+//int stacks = sphereAura.Stacks;
+//sphereAura.Material = auraMaterial;
+//sceneControl.Scene.SceneContainer.AddChild(sphereAura);
