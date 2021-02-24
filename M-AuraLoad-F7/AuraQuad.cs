cd99150ca31868cdba4bd807a3a88682186c8af3
moroz69off <moroz69off@gmail.com>
2021-02-24 13:35:24 +0300

    return 9

--- M-AuraLoad-F7/AuraQuad.cs
+++ M-AuraLoad-F7/AuraQuad.cs
@@ -44,50 +44,6 @@ namespace M_AuraLoad_F7
 
             GL.Flush();
             #endregion draw quads
-
         }
-
-        protected void calcNormal(float[,] v, float[] result)
-		{
-			float[] v1 = new float[3];
-			float[] v2 = new float[3];  // Vector 1 (x,y,z) & Vector 2 (x,y,z)
-			const int x = 0;                        // Define X Coord
-			const int y = 1;                        // Define Y Coord
-			const int z = 2;                        // Define Z Coord
-
-			// Finds The Vector Between 2 Points By Subtracting
-			// The x,y,z Coordinates From One Point To Another.
-
-			// Calculate The Vector From Point 1 To Point 0
-			v1[x] = v[0, x] - v[1, x];                  // Vector 1.x=Vertex[0].x-Vertex[1].x
-			v1[y] = v[0, y] - v[1, y];                  // Vector 1.y=Vertex[0].y-Vertex[1].y
-			v1[z] = v[0, z] - v[1, z];                  // Vector 1.z=Vertex[0].y-Vertex[1].z
-														// Calculate The Vector From Point 2 To Point 1
-			v2[x] = v[1, x] - v[2, x];                  // Vector 2.x=Vertex[0].x-Vertex[1].x
-			v2[y] = v[1, y] - v[2, y];                  // Vector 2.y=Vertex[0].y-Vertex[1].y
-			v2[z] = v[1, z] - v[2, z];                  // Vector 2.z=Vertex[0].z-Vertex[1].z
-														// Compute The Cross Product To Give Us A Surface Normal
-			result[x] = v1[y] * v2[z] - v1[z] * v2[y];              // Cross Product For Y - Z
-			result[y] = v1[z] * v2[x] - v1[x] * v2[z];              // Cross Product For X - Z
-			result[z] = v1[x] * v2[y] - v1[y] * v2[x];              // Cross Product For X - Y
-
-            ReduceToUnit(result);                       // Normalize The Vectors
-        }
-
-		protected void ReduceToUnit(float[] vector)
-		{                                   // To A Unit Normal Vector With A Length Of One.
-			float length;                           // Holds Unit Length
-
-			// Calculates The Length Of The Vector
-			length = (float)Math.Sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]) + (vector[2] * vector[2]));
-
-			if (length == 0.0f)                     // Prevents Divide By 0 Error By Providing
-				length = 1.0f;                      // An Acceptable Value For Vectors To Close To 0.
-
-			vector[0] /= length;                        // Dividing Each Element By
-			vector[1] /= length;                        // The Length Results In A
-			vector[2] /= length;                        // Unit Normal Vector.
-		}
-
 	}
 }
